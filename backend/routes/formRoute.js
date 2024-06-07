86b994fa0042f0cee30145247c2af8bcfbd5ef8e
Abhigya Dufare <76903966+abhigyadufare@users.noreply.github.com>
2024-06-07 16:16:33 +0530

    Delete backend directory

--- backend/routes/formRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createStudentProfile, getAllStudentProfile } from "../controller/studentForm.js";
-
-const router = express.Router();
-
-router.route("/form").post(createStudentProfile);
-router.route("/dashboard").get(getAllStudentProfile);
-
-export default router;
--- backend/server.js
+++
@@ -1,8 +0,0 @@
-import app from './app.js'
-import dbcon from './config/Database.js'
-// const dbconn = require('./config/Database.js')
-
-dbcon()
-app.listen(process.env.PORT, () => {
-    console.log(`Server on port ${process.env.PORT}`)
-})
